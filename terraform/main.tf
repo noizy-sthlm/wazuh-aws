8d7104ae623f3971dcfce74b7d33cb5db751bf6f
noizy-sthlm <57321964+noizy-sthlm@users.noreply.github.com>
2025-10-13 14:31:43 +0200

    Refactoring
    - VPC to vpc.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,3 @@
-
-# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/6.4.0
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.4.0"
-
-  name = var.project_name
-  cidr = var.vpc_cidr
-
-  azs             = [var.availability_zone]
-  public_subnets  = [var.public_subnet_cidr]
-  private_subnets = [var.private_subnet_cidr]
-
-  create_igw        = true
-  enable_nat_gateway = false
-
-  map_public_ip_on_launch = true
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    project = var.project_name
-  }
+provider "aws" {
+  region = var.region
 }
