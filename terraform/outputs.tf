c1ec25549aa495f92aa6bf94d77d199a3111c476
noizy-sthlm <57321964+noizy-sthlm@users.noreply.github.com>
2025-10-22 18:29:27 +0200

    Changed to outputs required for Ansible piping
        - Instance IDs
        - Instance IPs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "vpc_cidr"               { value = module.vpc.vpc_cidr_block }
-output "public_subnet_cidrs"    { value = module.vpc.public_subnets_cidr_blocks }
-output "private_subnet_cidrs"   { value = module.vpc.private_subnets_cidr_blocks }
+output "indexer_ip"     { value = module.wazuh_indexer.private_ip }
+output "server_ip"      { value = module.wazuh_server.private_ip }
+output "dashboard_ip"   { value = module.wazuh_dashboard.private_ip }
+
+output "indexer_instance_id"    { value = module.wazuh_indexer.id }
+output "server_instance_id"     { value = module.wazuh_server.id }
+output "dashboard_instance_id"  { value = module.wazuh_dashboard.id }
