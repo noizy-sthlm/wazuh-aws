ca371bf746de040aafc01386f58fc16d9f9873df
noizy-sthlm <57321964+noizy-sthlm@users.noreply.github.com>
2025-11-01 18:42:45 +0100

    Add Ansible playbooks
    - Add playbooks for Wazuh deployment
    - Generate Ansible inventory from terraform
    - Update terraform lock file and add local provider

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,22 @@
-output "vpc_cidr"               { value = module.vpc.vpc_cidr_block }
-output "public_subnet_cidrs"    { value = module.vpc.public_subnets_cidr_blocks }
-output "private_subnet_cidrs"   { value = module.vpc.private_subnets_cidr_blocks }
+output "indexer_ip"     { value = module.wazuh_indexer.private_ip }
+output "server_ip"      { value = module.wazuh_server.private_ip }
+output "dashboard_ip"   { value = module.wazuh_dashboard.private_ip }
+
+output "indexer_instance_id"    { value = module.wazuh_indexer.id }
+output "server_instance_id"     { value = module.wazuh_server.id }
+output "dashboard_instance_id"  { value = module.wazuh_dashboard.id }
+
+resource "local_file" "ansible_inventory" {
+  content  = templatefile("${path.module}/../ansible/inventory.tftpl",
+    {
+      ds_instance_id = module.wazuh_dashboard.id,
+      mngr_instance_id = module.wazuh_server.id,
+      idx_instance_id = module.wazuh_indexer.id,
+      ds_private_ip = module.wazuh_dashboard.private_ip,
+      mngr_private_ip = module.wazuh_server.private_ip,
+      idx_private_ip = module.wazuh_indexer.private_ip,
+    }
+  )
+  
+  filename = "${path.module}/../ansible/inventory.ini"
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,5 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.16.0"
     }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.0"
+    }
   }
 }
